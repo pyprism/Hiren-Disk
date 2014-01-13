408f6a997e8dbf17e5a11583b7bd22653edbe86d
pyprism <areose21@gmail.com>
2014-01-13 11:54:26 +0600

    update

--- Hiren_Disk/admin.py
+++ Hiren_Disk/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from Hiren_Disk.models import DiskBox, DiskList
 # Register your models here.
 
-admin.site.register([DiskBox,DiskList])
+class DiskBoxAdmin(admin.ModelAdmin):
+	list_display = ('diskBoxNo','diskName')
+	search_fields = ('diskName')
+
+admin.site.register([DiskBox,DiskList],DiskBoxAdmin,)
