aaa91401006150aa21f30688627eeb6e5d28c6dd
pyprism <git.pyprism@gmail.com>
2014-04-30 12:35:59 +0600

    TODO bug

--- disk/nisha.js
+++ disk/nisha.js
@@ -8,12 +8,13 @@ var exec = require('child_process').exec,
 /**
  * Get current user name
  */
-function User() {
-    exec('whoami',
-        function (error, stdout, stderr) {
-            return stdout;
+/*function User(cb) {
+    exec('whoami',function (error, stdout, stderr) {
+            if(error)
+            return cb(error);
+        cb(null,stdout);
         });
-}
+}*/
 
 /*
  Go to DVD and return contents names
@@ -25,8 +26,43 @@ module.exports = function Hiren() {
                 Hiren();
             else
                 pass;
-            process.chdir("/media/" + User() + '/' + stdout + "/");
+            exec('whoami',function(err , stdoutt , stderr){
+                process.chdir("/media/" + stdoutt + '/' + stdout + "/");
+            });
+            //process.chdir("/media/" + User() + '/' + stdout + "/");
             return fs.readdirSync(process.cwd());
         });
 }
 
+/*
+Eject DVD drive
+ */
+module.exports = function Eject(){
+    exec('eject',function(error , stdout,stderr){
+        if(error)
+        console.log(error);
+    });
+}
+
+function Eject(){
+    exec('eject',function(error , stdout,stderr){
+        if(error)
+            console.log(error);
+    });
+}
+
+function Hiren() {
+    exec('volname /dev/sr0',
+        function (error, stdout, stderr) {
+            if (stdout == ( "volname: No medium found" || "volname: Input/output error" ))
+                Hiren();
+            else {}
+            exec('whoami',function(err , user , stderr){
+                //process.chdir("/media/" + stdoutt + '/' + stdout + "/");
+                console.log("/media/" + user + "/" + stdout + "/");
+            });
+            return fs.readdirSync(process.cwd());
+        });
+}
+
+Hiren()
