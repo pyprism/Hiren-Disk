353e58d851b444be46a35325bccce08ddbf645d5
pyprism <git.pyprism@gmail.com>
2014-08-14 17:14:46 +0600

    add some more views

--- hiren/disk/views.py
+++ hiren/disk/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render , render_to_response
+from django.shortcuts import render, redirect
 from .models import Disk, Box
+from django.contrib import auth
 # Create your views here.
 
 
 def index(request):
-    return render_to_response('index.html', {'browse': "browse", "add": "x"})
+    return render(request, 'index.html', {'browse': "browse", "add": "x"})
 
 
 def browse(request):
     no = Box.objects.values('disk_no', 'created_at')
-    print(no)
-    return render_to_response('browse.html', { 'no' : no})
+    return render(request, 'browse.html', {'no' : no})
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('pass')
+        #print(email, password)
+        user = auth.authenticate(username=username, password=password)
+        print(user)
+        if user:
+            return redirect('/add')
+    else:
+        return redirect('/browse')
