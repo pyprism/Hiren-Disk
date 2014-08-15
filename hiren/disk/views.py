b7d26b2e1edf4736e23dc9857d9e5e91abc1b12c
pyprism <git.pyprism@gmail.com>
2014-08-15 21:04:51 +0600

    add new views

--- hiren/disk/views.py
+++ hiren/disk/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Disk, Box
 from django.contrib import auth
+from django.contrib import messages
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -10,7 +12,15 @@ def index(request):
 
 def browse(request):
     no = Box.objects.values('disk_no', 'created_at')
-    return render(request, 'browse.html', {'no' : no})
+    return render(request, 'browse.html', {'no': no})
+
+
+def disk_names(request, disk):
+    names = Disk.objects.filter(box_no=disk)
+    if names:
+        return render(request, 'names.html', {'names': names})
+    else:
+        return HttpResponse("Ops Something wrong happend") #TODO 404
 
 
 def login(request):
@@ -19,8 +29,22 @@ def login(request):
         password = request.POST.get('pass')
         #print(email, password)
         user = auth.authenticate(username=username, password=password)
-        print(user)
         if user:
             return redirect('/add')
+        else:
+            messages.error(request, 'Username/Password is not valid!')
+            return redirect(request.path)
     else:
-        return redirect('/browse')
+        return redirect("/browse")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/browse")
+
+
+def add(request):
+    if not request.user.is_authenticated():
+        return HttpResponse('logg')
+    else:
+        return HttpResponse("OK")
