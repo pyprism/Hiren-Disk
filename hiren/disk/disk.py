8e86ad3ee61f68a9275f0e98a20c695e92177a19
pyprism <git.pyprism@gmail.com>
2014-08-29 10:35:47 +0600

    Finally its working :smile:

--- hiren/disk/disk.py
+++ hiren/disk/disk.py
@@ -6,22 +6,31 @@ from .models import Box, Disk
 #TODO error handling and better response !
 
 
-def hiren(disk, box):
+def hiren():
     try:
         raw_mounted_dir = subprocess.check_output(["volname", "/dev/sr0"])
     except:
         pass
-    hiren = raw_mounted_dir.rstrip().decode("utf-8").lower().replace("_", " ")
-    #this will check the name of cd/dvd drive
-    dvdurl = ""
-    for drive_name in os.listdir("/media/"+getpass.getuser()+"/"):
-        if drive_name.lower() == hiren:
-            dvdurl = drive_name
-            break
+    if raw_mounted_dir:
+        hiren = raw_mounted_dir.rstrip().decode("utf-8").lower().replace("_", " ")
+        #this will check the name of cd/dvd drive
+        dvdurl = ""
+        for drive_name in os.listdir("/media/"+getpass.getuser()+"/"):
+            if drive_name.lower() == hiren:
+                dvdurl = drive_name
+                break
 
-    content = os.listdir("/media/"+getpass.getuser()+"/"+dvdurl)
-    b = Box(disk_no=box)
-    b.save()
-    a = Disk(serial=disk, title=content)
-    a.save()
-    print("x")
+        return os.listdir("/media/"+getpass.getuser()+"/"+dvdurl)
+
+
+def save_db(disk, box):
+    box_obj = Box.objects.filter(box_no=disk)
+    if box_obj:
+        a = Disk(disk_no=box_obj, serial=disk, contents=content)
+        a.save()
+    else:
+        b = Box(box_no=box)
+        b.save()
+        xoxo = Box.objects.get(box_no=box)   # need some optimization
+        a = Disk(disk_no=xoxo, serial=disk, contents=hiren())
+        a.save()
--- hiren/hiren/settings.py
+++ hiren/hiren/settings.py
@@ -61,6 +61,7 @@ WSGI_APPLICATION = 'hiren.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+       # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
