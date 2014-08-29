f029465976db98f0b72c5865d6944c5bc6655a10
pyprism <git.pyprism@gmail.com>
2014-08-29 10:37:24 +0600

    update names

--- hiren/disk/models.py
+++ hiren/disk/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Box(models.Model):
-    disk_no = models.IntegerField()
+    box_no = models.IntegerField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        #return str(self.disk_no)
-        return "%s %s" % (self.disk_no, self.created_at)
+        return "%s %s" % (self.box_no, self.created_at)
 
 
 class Disk(models.Model):
-    box_no = models.ForeignKey(Box)
-    serial = models.IntegerField()
-    titles = models.TextField()
+    disk_no = models.ForeignKey(Box)
+    serial = models.IntegerField(unique=True)
+    contents = models.TextField()
 
     def __str__(self):
-        return '%s %s' % (self.serial, self.titles)
+        return '%s %s' % (self.serial, self.contents)
