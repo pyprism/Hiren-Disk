5f555afad03a8043cb72a94f6d815aced9e8f04d
pyprism <git.pyprism@gmail.com>
2014-09-02 19:04:37 +0600

    removed some useless lines

--- hiren/disk/models.py
+++ hiren/disk/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from djorm_pgfulltext.models import SearchManager
-from djorm_pgfulltext.fields import VectorField
 
 
 class Box(models.Model):
@@ -15,15 +13,6 @@ class Disk(models.Model):
     disk_no = models.ForeignKey(Box)
     serial = models.IntegerField(unique=True)
     contents = models.TextField()
-
-    search_index = VectorField()
-
-    objects = SearchManager(
-        fields=contents,
-        config='pg_catalog.english',
-        search_field='search_index',
-        auto_update_search_field=True
-    )
 
     def __str__(self):
         return '%s %s' % (self.serial, self.contents)
