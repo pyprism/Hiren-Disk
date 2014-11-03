fd1c25e64324c6a599a3fb69d4c765014d32602a
pyprism <git.pyprism@gmail.com>
2014-11-03 22:15:42 +0600

    added new routes

--- hiren/hiren/urls.py
+++ hiren/hiren/urls.py
@@ -17,4 +17,6 @@ urlpatterns = patterns('',
     url(r"^json$", 'disk.views.json'),
     url(r"^eject$", 'disk.views.eject'),
     url(r"^browse/(?P<disk>\d+)/$", 'disk.views.disk_names'),
+    url(r"^browse/id/(?P<ids>\d+)/edit$", 'disk.views.edit'),
+    url(r"^browse/id/(?P<ids>\d+)/delete$", 'disk.views.delete'),
 )
